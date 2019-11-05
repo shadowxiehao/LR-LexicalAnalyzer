cafda916b65dbfa925db16d88f34ce52bd615aa0
谢昊 <799522476@qq.com>
2019-11-05 09:41:03 +0800

    2019.11.5 9:40 进一步实现中

--- src/LR.java
+++ src/LR.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class LR {
@@ -22,7 +24,7 @@ public class LR {
 8	r2	r2	　	r2	　	r2	　
 9	r3	r3	　	r3	　	r3	　
      */
-    int[][] table = {{-1,-1,2,-1,3,-1,1},
+    private int[][] table = {{-1,-1,2,-1,3,-1,1},
                      {4,5,-1,-1,-1,0,-1},
                      {-1,-1,2,-1,3,-1,6},
                      {104,104,-1,104,-1,104,-1},
@@ -33,8 +35,118 @@ public class LR {
                      {102,102,-1,102,-1,102,-1},
                      {103,103,-1,103,-1,103,-1},
     };
-    Stack stack = new Stack();
+    private Stack symbol_stack = new Stack();//符号栈
+    private Stack state_stack = new Stack();//状态栈
+    private String input = null; //输入串,初始化时默认加上#结尾
+    private int state=0;
+
+    int index=0;//记录当前读到字符串第几个字符
+    HashMap<String,Integer> keywords = new HashMap<String,Integer>();
+
+    public LR (String input){//初始化分析
+        this.input=input+"#";//输入串加上'#' 初始化输入串
+        init_keywords();
+    }
+    private int init_keywords(){//将输入符号 转换成 LR分析表中的对应列数
+        keywords.put("+",0);
+        keywords.put("*",1);
+        keywords.put("(",2);
+        keywords.put(")",3);
+        keywords.put("i",4);
+        keywords.put("#",5);
+        keywords.put("E",6);
+
+        return 0;//正常
+    }
+
+    public void show(){
+        String symbol = null;//记录找到的预设符号
+        int Do = -1;//记录对应的表格内的数字
+        try{
+            state_stack.push(state);
+            symbol = find();
+            symbol_stack.push(symbol);
+            Do = table[state][keywords.get(symbol)];
+
+            if(Do<0){
+                throw new Exception("出错!分析表没有对应符号处理");
+            }
+            else if(Do>0&&Do<=100){
+
+            }
+            else if(Do>100){
+
+            }
 
 
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+    private String find() throws Exception{//找到之后是"+","*",...中的哪一个,都不属于则报错
+        int num_flag=0;
+        String next=null;//next 代表整个字符串里 下一个有效的输入 (标识符,符号)
+        if(isDigit(input.charAt(index))){
+            index++;
+            while (isDigit(input.charAt(index))){ index++; }
+            return "i";
+        }
+        else if(isLetter(input.charAt(index))){
+            index++;
+            int flag=0;
+
+            while (isLetter(input.charAt(index))){ index++; flag++;}
+
+            if(flag==0){
+                if(keywords.containsKey(input.charAt(index-1))){
+                    return input.charAt(index-1)+"";
+                }
+            }
+            return "i";
+        }
+        else if(keywords.containsKey(input.charAt(index))){
+            index++;
+            return input.charAt(index)+"";
+        }
+        else {
+            throw new Exception("第"+(index+1)+"个输入字符为非法输入!");
+        }
+    }
+
+
+    //判断是否是字母
+    boolean isLetter(char letter)
+    {
+        return (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z');
+    }
+    //判断是否是数字
+    boolean isDigit(char digit)
+    {
+        return digit >= '0' && digit <= '9';
+    }
+    //判断是否是$与下划线
+    boolean is$_(char letter)
+    {
+        return (letter == '$') || (letter == '_');
+    }
+    boolean isComment1_s(char letter)
+    {
+        return (letter == '$') || (letter == '_');
+    }
+    boolean isComment1_e(char letter)
+    {
+        return (letter == '$') || (letter == '_');
+    }
+    boolean isComment2_s(char letter)
+    {
+        return (letter == '$') || (letter == '_');
+    }
+    boolean isComment2_e(char letter)
+    {
+        return (letter == '$') || (letter == '_');
+    }
 
 }
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String arg[]){
+
+        System.out.println("Please enter an arithmetic expression to analyse!");
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        String expression;
+        try {
+            expression = in.readLine();//读入输入
+            LR test=new LR(expression);
+            test.show();//开始分析
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("输入出错！");
+        }
+
+    }
+}
